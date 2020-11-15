8f59ef562d7bd22b439cfb69c0d2addec9546a27
Guilherme Rodrigues <guilherme.araujo@grancursosonline.com.br>
2020-11-15 11:55:19 -0300

    Add Project

---
+++ 3-microservices/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "1. Launch an ECS cluster using the Cloudformation template"
+
+aws cloudformation deploy \
+--template-file infrastructure/ecs.yml \
+--region us-east-1 \
+--stack-name ecs-node \
+--capabilities CAPABILITY_NAMED_IAM
+
+echo "2. Deploy the services onto your cluster"
+
+./deploy us-east-1 ecs-node
